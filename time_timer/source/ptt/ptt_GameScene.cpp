29b6312feb94c186a37589376c4c8b95a0400f39
r2road <studior2road@gmail.com>
2022-08-13 22:32:51 +0900

    update scene : ptt - GameScene -  show guide

--- time_timer/source/ptt/ptt_GameScene.cpp
+++ time_timer/source/ptt/ptt_GameScene.cpp
@@ -17,6 +17,7 @@
 #include "r2bix/r2node_PivotNode.h"
 #include "r2bix/r2node_SpriteNode.h"
 
+#include "ptt/ptt_Config.h"
 #include "ptt/ptt_TextureTable.h"
 
 namespace ptt
@@ -42,6 +43,22 @@ namespace ptt
 					( director.GetScreenBufferSize().GetWidth() * 0.5f )
 					, ( director.GetScreenBufferSize().GetHeight() * 0.11f )
 				);
+			}
+
+			//
+			// Debug
+			//
+			if( ptt::Config::GetNodeConfig().pivot )
+			{
+				//
+				//
+				//
+				ret->AddChild<r2node::PivotNode>( std::numeric_limits<int>::max() );
+				ret->AddChild<r2node::PivotNode>( std::numeric_limits<int>::max() )->mTransformComponent->SetPosition( director.GetScreenBufferSize().GetWidth() - 1, 0 );
+				ret->AddChild<r2node::PivotNode>( std::numeric_limits<int>::max() )->mTransformComponent->SetPosition( director.GetScreenBufferSize().GetWidth() - 1, director.GetScreenBufferSize().GetHeight() - 1 );
+				ret->AddChild<r2node::PivotNode>( std::numeric_limits<int>::max() )->mTransformComponent->SetPosition( 0, director.GetScreenBufferSize().GetHeight() - 1 );
+
+				ret->AddChild<r2node::PivotNode>( std::numeric_limits<int>::max() )->mTransformComponent->SetPosition( director.GetScreenBufferSize().GetWidth() * 0.5f, director.GetScreenBufferSize().GetHeight() * 0.5f );
 			}
 		}
 
