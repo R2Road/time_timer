7f159b03eb4e3c155a6a827fb16fc47949022044
r2road <studior2road@gmail.com>
2022-08-16 01:57:24 +0900

    update class : ptt - Timer - use std::chrono::nanoseconds

--- time_timer/source/ptt/ptt_Timer.cpp
+++ time_timer/source/ptt/ptt_Timer.cpp
@@ -3,15 +3,15 @@
 namespace ptt
 {
 	Timer::Timer() :
-		mCurrentTime( std::chrono::milliseconds( 0 ) )
-		, mLastTime( std::chrono::milliseconds( 0 ) )
-		, mElapsedDuration( std::chrono::milliseconds( 0 ) )
+		mCurrentTime( std::chrono::nanoseconds( 0 ) )
+		, mLastTime( std::chrono::nanoseconds( 0 ) )
+		, mElapsedDuration( std::chrono::nanoseconds( 0 ) )
 	{}
 
 	void Timer::Start()
 	{
 		mLastTime = mCurrentTime = std::chrono::high_resolution_clock::now();
-		mElapsedDuration = std::chrono::duration_cast<std::chrono::milliseconds>( mCurrentTime - mLastTime );
+		mElapsedDuration = std::chrono::duration_cast<std::chrono::nanoseconds>( mCurrentTime - mLastTime );
 	}
 	void Timer::Update()
 	{
@@ -23,6 +23,6 @@ namespace ptt
 		mLastTime = mCurrentTime;
 		mCurrentTime = std::chrono::high_resolution_clock::now();
 
-		mElapsedDuration += std::chrono::duration_cast<std::chrono::milliseconds>( mCurrentTime - mLastTime );
+		mElapsedDuration += std::chrono::duration_cast<std::chrono::nanoseconds>( mCurrentTime - mLastTime );
 	}
 }
