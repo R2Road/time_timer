c35abba150452611d7f4b9e42efed61bac797d7b
r2road <studior2road@gmail.com>
2022-08-20 17:57:31 +0900

    update node : ptt - IndicatorNode - apply new texture and animation

--- time_timer/source/ptt/ptt_IndicatorNode.cpp
+++ time_timer/source/ptt/ptt_IndicatorNode.cpp
@@ -1,9 +1,11 @@
 #include "ptt_IndicatorNode.h"
 
 #include "r2bix/r2component_TextureFrameRenderComponent.h"
+#include "r2bix/r2component_TextureFrameAnimationComponent.h"
 #include "r2bix/r2node_PivotNode.h"
 
 #include "ptt_Config.h"
+#include "ptt_TextureFrameAnimationTable.h"
 #include "ptt_TextureTable.h"
 
 namespace ptt
@@ -17,8 +19,41 @@ namespace ptt
 			//
 			//
 			{
-				auto tfrc = ret->AddComponent<r2component::TextureFrameRenderComponent>();
-				tfrc->SetTextureFrame( ptt::TextureTable::GetInstance().GetTextureFrame( "indicator_0" ) );
+				{
+					auto arrow_node = ret->AddChild<r2base::Node>();
+
+					//
+					arrow_node->GetComponent<r2component::TransformComponent>()->SetPositionY( -6 );
+
+					//
+					auto texture_frame_render_component = arrow_node->AddComponent<r2component::TextureFrameRenderComponent>();
+
+					//
+					auto texture_frame_animation_component = arrow_node->AddComponent<r2component::TextureFrameAnimationComponent>();
+					texture_frame_animation_component->LoadAnimation( ptt::TextureFrameAnimationTable::GetInstance().Get( 2 ) );
+
+					//
+					texture_frame_animation_component->SetTextureFrameRenderComponent( texture_frame_render_component );
+					texture_frame_animation_component->RunAnimation_Repeat( r2animation::eIndex::Idle_0 );
+				}
+
+				{
+					auto arrow_node = ret->AddChild<r2base::Node>();
+
+					//
+					arrow_node->GetComponent<r2component::TransformComponent>()->SetPositionY( 7 );
+
+					//
+					auto texture_frame_render_component = arrow_node->AddComponent<r2component::TextureFrameRenderComponent>();
+
+					//
+					auto texture_frame_animation_component = arrow_node->AddComponent<r2component::TextureFrameAnimationComponent>();
+					texture_frame_animation_component->LoadAnimation( ptt::TextureFrameAnimationTable::GetInstance().Get( 3 ) );
+
+					//
+					texture_frame_animation_component->SetTextureFrameRenderComponent( texture_frame_render_component );
+					texture_frame_animation_component->RunAnimation_Repeat( r2animation::eIndex::Idle_0 );
+				}
 			}
 
 			//
