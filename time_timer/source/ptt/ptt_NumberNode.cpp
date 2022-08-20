de9fdaa4151727c97d9e895273d5fc2104b0832b
r2road <studior2road@gmail.com>
2022-08-20 17:35:15 +0900

    debug

--- time_timer/source/ptt/ptt_NumberNode.cpp
+++ time_timer/source/ptt/ptt_NumberNode.cpp
@@ -2,7 +2,9 @@
 
 #include "r2bix/r2component_TextureFrameAnimationComponent.h"
 #include "r2bix/r2component_TextureFrameRenderComponent.h"
+#include "r2bix/r2node_PivotNode.h"
 
+#include "ptt_Config.h"
 #include "ptt_TextureFrameAnimationTable.h"
 #include "ptt_TextureTable.h"
 #include "ptt_NumberComponent.h"
@@ -14,15 +16,28 @@ namespace ptt
 		auto ret( r2base::Node::Create( director ) );
 		if( ret )
 		{
-			auto frame_render_component = ret->AddComponent<r2component::TextureFrameRenderComponent>();
-			frame_render_component->SetTextureFrame( ptt::TextureTable::GetInstance().GetTextureFrame( "number_0" ) );
+			//
+			//
+			//
+			{
+				auto frame_render_component = ret->AddComponent<r2component::TextureFrameRenderComponent>();
+				frame_render_component->SetTextureFrame( ptt::TextureTable::GetInstance().GetTextureFrame( "number_0" ) );
 
-			auto frame_animation_component = ret->AddComponent<r2component::TextureFrameAnimationComponent>();
-			frame_animation_component->SetTextureFrameRenderComponent( frame_render_component );
-			frame_animation_component->LoadAnimation( ptt::TextureFrameAnimationTable::GetInstance().Get( 1 ) );
+				auto frame_animation_component = ret->AddComponent<r2component::TextureFrameAnimationComponent>();
+				frame_animation_component->SetTextureFrameRenderComponent( frame_render_component );
+				frame_animation_component->LoadAnimation( ptt::TextureFrameAnimationTable::GetInstance().Get( 1 ) );
 
-			auto number_component = ret->AddComponent<ptt::NumberComponent>();
-			number_component->SetTextureFrameAnimationComponent( frame_animation_component );
+				auto number_component = ret->AddComponent<ptt::NumberComponent>();
+				number_component->SetTextureFrameAnimationComponent( frame_animation_component );
+			}
+
+			//
+			// Debug
+			//
+			if( ptt::Config::GetNodeConfig().pivot )
+			{
+				ret->AddChild<r2node::PivotNode>();
+			}
 		}
 
 		return ret;
