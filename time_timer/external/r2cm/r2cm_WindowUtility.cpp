139cdcb21fc7459df5378330eb8b99d67f809420
r2road <studior2road@gmail.com>
2022-08-21 13:40:18 +0900

    update r2cm

--- time_timer/external/r2cm/r2cm_WindowUtility.cpp
+++ time_timer/external/r2cm/r2cm_WindowUtility.cpp
@@ -44,6 +44,12 @@ namespace r2cm
 	{
 		ShowScrollBar( GetConsoleWindow(), SB_VERT, visible );
 	}
+	void WindowUtility::MaximizeButtonVisible( const bool visible )
+	{
+		LONG window_style = GetWindowLong( GetConsoleWindow(), GWL_STYLE );
+		window_style = ( visible ? window_style | ( WS_MAXIMIZEBOX ) : window_style & ~( WS_MAXIMIZEBOX ) );
+		SetWindowLong( GetConsoleWindow(), GWL_STYLE, window_style );
+	}
 
 	WindowUtility::CursorPoint WindowUtility::GetCursorPoint()
 	{
