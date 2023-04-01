76684e67a997d9d22ea777ec6dfc7e8948376891
R2Road <studior2road@gmail.com>
2023-04-01 17:20:40 +0900

    ...

--- time_timer/source/ptt/ptt_VersionInfo.h
+++ time_timer/source/ptt/ptt_VersionInfo.h
@@ -11,12 +11,19 @@ namespace ptt
 
 	static struct
 	{
+		const char* const String4SubjectAsAProgrammer = {
+					"### 프로젝트 목표 | Subject Of A Project ###"
+			"\n"
+			"\n"	"[o] "    "전반의 데이터를 다루는 Core 객체를 둔다"
+			"\n"	"[o] "    "Core 객체에 Singleton은 사용하지 않는다"
+			"\n"	"[o] "    "Scene 과 함께 이동한다."
+		};
+
+
 
 		const char VersionNumber_1 = '1';
 		const char VersionNumber_2 = '0';
 		const char VersionNumber_3 = '7';
-
-
 
 		const char String4Version[19] = { 'T', 'i', 'm', 'e', 'T', 'i', 'm', 'e', 'r', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '\0' };
 
@@ -38,16 +45,6 @@ namespace ptt
 			"\n"	"[o] " "Add Interface 2 Alram Scene : 15분 더"
 			"\n"	"[o] " "Add Interface 2 Alram Scene : 20분 더"
 		;
-
-
-
-		const char* const String4SubjectAsAProgrammer = {
-					"### 프로젝트 목표 | Subject Of A Project ###"
-			"\n"
-			"\n"	"[o] "    "전반의 데이터를 다루는 Core 객체를 둔다"
-			"\n"	"[o] "    "Core 객체에 Singleton은 사용하지 않는다"
-			"\n"	"[o] "    "Scene 과 함께 이동한다."
-		};
 
 	} VersionInfo;
 }
