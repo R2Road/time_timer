c108aee25ab238f9ce4b0b7b126394f759fb08a5
R2Road <studior2road@gmail.com>
2023-04-01 17:30:38 +0900

    add binary : v1.0.8

--- time_timer/source/ptt/ptt_VersionInfo.h
+++ time_timer/source/ptt/ptt_VersionInfo.h
@@ -23,7 +23,7 @@ namespace ptt
 
 		const char VersionNumber_1 = '1';
 		const char VersionNumber_2 = '0';
-		const char VersionNumber_3 = '7';
+		const char VersionNumber_3 = '8';
 
 		const char String4Version[19] = { 'T', 'i', 'm', 'e', 'T', 'i', 'm', 'e', 'r', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '\0' };
 
@@ -32,7 +32,6 @@ namespace ptt
 		const char* const String4Road2Version_1_0_8 =
 					"### Road 2 Version 1.0.8 ###"
 			"\n"
-			"\n"	"[ ] " "..."
 			"\n"	"[o] " "update timer scene : improve key info - show resume key info"
 			"\n"	"[o] " "update alram scene : adjust additional time - 5, 10, 15, 20 > 1, 5, 10, 15"
 		;
